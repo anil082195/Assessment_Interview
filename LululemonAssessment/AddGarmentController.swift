d02471d9d9837004d750c6e16a64e2f09a4f5c76
Anil Keshwar Persaud <Anil.keshwar@gmail.com>
2022-06-29 11:18:48 -0400

    Initial commit

---
+++ LululemonAssessment/AddGarmentController.swift
@@ -0,0 +1,55 @@
+//
+//  AddGarmentController.swift
+//  Assessment
+//
+//  Created by Anil Persaud on 24/06/22.
+//  Copyright © 2022 Anil Persaud. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+protocol AddGarmentProtocol {
+    func updateTableData()
+}
+
+class AddGarmentController: UIViewController {
+    
+    @IBOutlet weak private var txtGarment: UITextField!
+    var addGarmentProtocol: AddGarmentProtocol!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Add Garment"
+        
+
+    }
+    
+    @IBAction func saveGarment(_sender: Any) {
+        if let txtGarment = txtGarment.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            if txtGarment.count == 0 {
+                showAlert()
+                return
+            } else {
+                let context = CoredataManager.shared.context
+                guard let entity = NSEntityDescription.entity(forEntityName: "Garments", in: context) else { return }
+                let garmentModel = NSManagedObject(entity: entity, insertInto: context) as? Garments
+                garmentModel?.garmentName = txtGarment
+                garmentModel?.createdTime = Date()
+                do {
+                    try context.save()
+                    addGarmentProtocol.updateTableData()
+                    self.dismiss(animated: true, completion: nil)
+                } catch {
+                    print("Failed Saving")
+                }
+            }
+        }
+    }
+    
+    private func showAlert() {
+        let alertController = UIAlertController(title: "Alert", message: "Please enter garment name", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
